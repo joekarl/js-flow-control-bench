48e915fab7b3eae5a9f67c4ee2020104d07c3541
Karl Kirch <kkirch@wdtinc.com>
2014-06-02 14:32:08 -0500

    Initial commit of benchmark

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ js-flow-control-bench
 =====================
 
 Various benchmarks of js flow control libraries
+
+Shamelessly borrowed from https://github.com/petkaantonov/bluebird
+
+##Run benchmarks
+There are 3 benchmarks:
+
+* `./bench parallel` - contrived simple loop benchmark
+* `./bench doxbee` - Run simulated mixed task flow benchmark
+* `./bench doxbee-errors` - Run simulated mixed task flow benchmark with 10% errors
---
+++ madeup-parallel/promises-getify-native-promise-only.js
@@ -0,0 +1,19 @@
+var NPO_Promise = require('native-promise-only');
+require('../lib/fakesP');
+
+module.exports = function upload(stream, idOrPath, tag, done) {
+    var queries = new Array(global.parallelQueries);
+    var tx = db.begin();
+
+    for( var i = 0, len = queries.length; i < len; ++i ) {
+        queries[i] = FileVersion.insert({index: i}).execWithin(tx);
+    }
+
+    NPO_Promise.all(queries).then().then(function() {
+        tx.commit();
+        done();
+    }, function(err) {
+        tx.rollback();
+        done(err);
+    });
+}
